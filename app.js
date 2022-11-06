d282f6ff5f13805c5628a979b5950c474694287e
Wenh0 <117516008+Wenh0@users.noreply.github.com>
2022-11-06 16:25:27 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const sr = ScrollReveal({
+    duration: 1000,
+    reset: true
+});
+
+sr.reveal('.subcontent' , {
+    origin : 'bottom',
+    duration : 1000,
+});
+
+sr.reveal('.click', {
+    delay : 500
+})
+
+sr.reveal('.ul', {
+
+})
+
